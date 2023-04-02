ecc7bac37978230471410eac85db324743b3220d
mirasac <50930731+mirasac@users.noreply.github.com>
2023-04-02 16:56:18 +0200

    Use precomputed positions for graph drawing

--- Project/main.py
+++ Project/main.py
@@ -5,18 +5,22 @@ def main():
     path_compsist = 'decks_network.gz'
     G = nx.read_edgelist(path_compsist, delimiter=',', comments=None)
     print(f"Original graph: {G}")
-    G.remove_nodes_from([
+    basic_lands = [
         "Plains",
         "Island",
         "Swamp",
         "Mountain",
         "Forest"
-    ])
+    ]
+    G.remove_nodes_from(basic_lands)
     print(f"Without basic lands: {G}")
 
     # Draw graph.
     plt.figure()
-    nx.draw_spring(G, with_labels=True, node_size=5)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos=pos, with_labels=False, node_size=5)
+    plt.show()
+    nx.draw(G, pos=pos, with_labels=True, node_size=5)
     plt.show()
 
 main()
